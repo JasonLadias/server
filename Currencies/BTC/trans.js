94a81235be4c5946059058152910404406fc2598
Jason <jason.ladias@gmail.com>
2020-06-10 14:02:23 +0300

    btc multiple tries

--- Currencies/BTC/trans.js
+++ Currencies/BTC/trans.js
@@ -7,6 +7,7 @@ let logger = require('../../Logging/logger')
 
 //will send the BTC transaction having as input the number, receiving address & amount
 exports.trans = async (addressNo, addressTo, value) => {
+    let error = [1,2,3,4,5,6,7,8,9,10]
     let path = "BTC/Trans"
     //retrieving BTC address & privateKey for the specific number
     let address = btcWallet.wallet(addressNo)
@@ -22,10 +23,25 @@ exports.trans = async (addressNo, addressTo, value) => {
     let WIF = wif.encode(128, privateKey, true)
 
     //we use promise to get asyncronous results, resolve in promise works as a return in a normal function
+    let i = 0, status
+    while(i<6 ){
+        if(i%2 == 0){
+            status = await server1(address, addressTo, amount, fee, WIF, path, value)
+        }else{
+            status = await server2(address, addressTo, amount, fee, WIF, path, value)
+        }
+        if(!error.includes(status)) break
+        i++
+    }
+
+    return status
+
+}
+
+const server1 = async (address, addressTo, amount, fee, WIF, path, value) => {
+
     let promise = new Promise((resolve, reject) => {
         //getting UTXOs for the specific address
-
-
         axios({
             method: 'post',
             url: 'https://insight.bitpay.com/api/addrs/utxo',
@@ -38,7 +54,8 @@ exports.trans = async (addressNo, addressTo, value) => {
                 if (res.data) {
                     let utxos = res.data
 
-                    if(!value){
+
+                    if (!value) {
                         amount = -fee
                         for (let i = 0; i < utxos.length; i++) {
                             amount += utxos[i]['satoshis']
@@ -61,7 +78,7 @@ exports.trans = async (addressNo, addressTo, value) => {
                             "data": tx
                         })
                     }
-                
+
                     axios(req)
                         .then((res) => {
                             logger.log(path, JSON.stringify(req), JSON.stringify(res.data))
@@ -92,20 +109,93 @@ exports.trans = async (addressNo, addressTo, value) => {
                     resolve(6)
                 }
             })
-
-
-
     })
 
-
-
-
-    let status = await promise
-
-    return status
-
+    return promise
 }
 
+
+const server2 = async (address, addressTo, amount, fee, WIF, path, value) => {
+
+    let promise = new Promise((resolve, reject) => {
+        //getting UTXOs for the specific address
+        axios.get('https://blockchain.info/unspent?cors=true&active=' + address)
+            .then((res) => {
+
+                if (res.data.unspent_outputs) {
+                    let utxos = res.data.unspent_outputs
+                    
+                    let utxo = []
+                    for (let i = 0; i < utxos.length; i++) {
+                        utxo = [
+                            ...utxo,
+                            {
+                                "txid": utxos[i]['tx_hash_big_endian'],
+                                "vout": utxos[i]['tx_output_n'],
+                                "scriptPubKey": utxos[i]['script'],
+                                "satoshis": utxos[i]['value']
+                            }
+                        ]
+                    }
+                    
+                    if (!value) {
+                        amount = -fee
+                        for (let i = 0; i < utxos.length; i++) {
+                            amount += utxos[i]['satoshis']
+                        }
+                    }
+
+                    let tx = new bitcore.Transaction() //use bitcore-lib-cash to create a transaction
+                        .from(utxo)
+                        .to(addressTo, amount)
+                        .fee(fee)
+                        .change(address)
+                        .sign(WIF)
+                        .serialize()
+
+                    let req = {
+                        method: 'post',
+                        url: 'https://api.blockchair.com/bitcoin/push/transaction',
+                        headers: { "Content-Type": "application/json" },
+                        data: JSON.stringify({
+                            "data": tx
+                        })
+                    }
+
+                    axios(req)
+                        .then((res) => {
+                            logger.log(path, JSON.stringify(req), JSON.stringify(res.data))
+                            if (res.data.data.transaction_hash) {
+                                resolve(res.data.data.transaction_hash)
+                            } else {
+                                resolve(8)
+                            }
+                        })
+                        .catch((err) => {
+                            if (err.isAxiosError) {
+                                logger.log(path, JSON.stringify(req), JSON.stringify(err.data))
+                                resolve(7)
+                            } else {
+                                logger.log(path, JSON.stringify(req), err)
+                                resolve(9)
+                            }
+                        })
+
+                } else {
+                    resolve(5)
+                }
+            })
+            .catch((err) => {
+                if (err.isAxiosError) {
+                    resolve(4)
+                } else {
+                    resolve(6)
+                }
+            })
+    })
+
+    return promise
+}
 
 /*
 axios.get('https://blockchain.info/unspent?cors=true&active=' + address)
