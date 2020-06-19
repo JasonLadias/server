6852450b6a0e82cdd982c48a10ce1402e34185a8
Jason <jason.ladias@gmail.com>
2020-06-19 18:52:42 +0300

    response from trans.js

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -9,7 +9,19 @@ const eosTrans = require('../Currencies/EOS/trans')
 
 exports.postTransaction = async (req, res, next) => {
     //Geting the coinName from the POST request
-    let error = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    let error = {
+        1 : 'Bad Endpoint Request',
+        2 : 'Address Number is not valid',
+        3 : 'Coin Name does not exist',
+        4 : 'UTXO server down',
+        5 : 'UTXO Server unexpected response',
+        6 : 'Transaction build error',
+        7 : 'Broadcast server down',
+        8 : 'Broadcast server unexpected response ***Possible send of money***',
+        9 : 'Broadcast server unreadable response ***Possible send of money***',
+        10 : 'Value not set',
+        11 : 'Amount not available',
+    }
     let coinName = req.body.coinName
     let addressNo = req.body.addressNo
     let addressTo = req.body.addressTo
@@ -21,115 +33,112 @@ exports.postTransaction = async (req, res, next) => {
         case 'BTC':
             //BTC transaction
             status = await btcTrans.trans(addressNo, addressTo, value)
-            if (error.includes(status)) {
-                res.send(JSON.stringify({ status: "Error", type: status, reason: errorType(status) }))
-            } else {
-                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status }))
+            if(status.status == 'OK'){
+                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status.tx }))
+            }else if(error[status]){
+                res.send(JSON.stringify({ status: "Error", type: status, reason: error[status] }))
+            }else{
+                res.send(JSON.stringify({ status: "Error", type: 12, reason: "Something unexpected happened" }))
             }
             break
         case 'BSV':
             //BSV transaction
             status = await bsvTrans.trans(addressNo, addressTo, value)
-            if (error.includes(status)) {
-                res.send(JSON.stringify({ status: "Error", type: status, reason: errorType(status) }))
-            } else {
-                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status }))
+            if(status.status == 'OK'){
+                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status.tx }))
+            }else if(error[status]){
+                res.send(JSON.stringify({ status: "Error", type: status, reason: error[status] }))
+            }else{
+                res.send(JSON.stringify({ status: "Error", type: 12, reason: "Something unexpected happened" }))
             }
             break
         case 'BCH':
             //BCH transaction
             status = await bchTrans.trans(addressNo, addressTo, value)
-            if (error.includes(status)) {
-                res.send(JSON.stringify({ status: "Error", type: status, reason: errorType(status) }))
-            } else {
-                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status }))
+            if(status.status == 'OK'){
+                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status.tx }))
+            }else if(error[status]){
+                res.send(JSON.stringify({ status: "Error", type: status, reason: error[status] }))
+            }else{
+                res.send(JSON.stringify({ status: "Error", type: 12, reason: "Something unexpected happened" }))
             }
             break
         case 'LTC':
             //LTC transaction
             status = await ltcTrans.trans(addressNo, addressTo, value)
-            if (error.includes(status)) {
-                res.send(JSON.stringify({ status: "Error", type: status, reason: errorType(status) }))
-            } else {
-                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status }))
+            if(status.status == 'OK'){
+                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status.tx }))
+            }else if(error[status]){
+                res.send(JSON.stringify({ status: "Error", type: status, reason: error[status] }))
+            }else{
+                res.send(JSON.stringify({ status: "Error", type: 12, reason: "Something unexpected happened" }))
             }
             break
         case 'DGB':
             //DGB transaction (not working)
             status = await dgbTrans.trans(addressNo, addressTo, value)
-            if (error.includes(status)) {
-                res.send(JSON.stringify({ status: "Error", type: status, reason: errorType(status) }))
-            } else {
-                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status }))
+            if(status.status == 'OK'){
+                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status.tx }))
+            }else if(error[status]){
+                res.send(JSON.stringify({ status: "Error", type: status, reason: error[status] }))
+            }else{
+                res.send(JSON.stringify({ status: "Error", type: 12, reason: "Something unexpected happened" }))
             }
             break
         case 'ETH':
             status = await ethTrans.trans(addressNo, addressTo, value)
-            if (error.includes(status)) {
-                res.send(JSON.stringify({ status: "Error", type: status, reason: errorType(status) }))
-            } else {
-                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status }))
+            if(status.status == 'OK'){
+                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status.tx }))
+            }else if(error[status]){
+                res.send(JSON.stringify({ status: "Error", type: status, reason: error[status] }))
+            }else{
+                res.send(JSON.stringify({ status: "Error", type: 12, reason: "Something unexpected happened" }))
             }
             break
         case 'EOS':
             memo = req.body.memo
             status = await eosTrans.trans(addressTo, value, memo)
-            if (error.includes(status)) {
-                res.send(JSON.stringify({ status: "Error", type: status, reason: errorType(status) }))
-            } else {
-                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status }))
+            if(status.status == 'OK'){
+                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status.tx }))
+            }else if(error[status]){
+                res.send(JSON.stringify({ status: "Error", type: status, reason: error[status] }))
+            }else{
+                res.send(JSON.stringify({ status: "Error", type: 12, reason: "Something unexpected happened" }))
             }
             break
-
         //ETH Tokens
         case 'POWR':
             status = await ethTokenTrans.trans(addressNo, addressTo, value, '0x595832f8fc6bf59c85c527fec3740a1b7a361269', 6, 'POWR')
-            if (error.includes(status)) {
-                res.send(JSON.stringify({ status: "Error", type: status, reason: errorType(status) }))
-            } else {
-                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status }))
+            if(status.status == 'OK'){
+                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status.tx }))
+            }else if(error[status]){
+                res.send(JSON.stringify({ status: "Error", type: status, reason: error[status] }))
+            }else{
+                res.send(JSON.stringify({ status: "Error", type: 12, reason: "Something unexpected happened" }))
             }
             break
         case 'OMG':
             status = await ethTokenTrans.trans(addressNo, addressTo, value, '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07', 18, 'OMG')
-            if (error.includes(status)) {
-                res.send(JSON.stringify({ status: "Error", type: status, reason: errorType(status) }))
-            } else {
-                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status }))
+            if(status.status == 'OK'){
+                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status.tx }))
+            }else if(error[status]){
+                res.send(JSON.stringify({ status: "Error", type: status, reason: error[status] }))
+            }else{
+                res.send(JSON.stringify({ status: "Error", type: 12, reason: "Something unexpected happened" }))
             }
             break
         case 'LEND':
             status = await ethTokenTrans.trans(addressNo, addressTo, value, '0x80fB784B7eD66730e8b1DBd9820aFD29931aab03', 18, 'LEND')
-            if (error.includes(status)) {
-                res.send(JSON.stringify({ status: "Error", type: status, reason: errorType(status) }))
-            } else {
-                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status }))
+            if(status.status == 'OK'){
+                res.send(JSON.stringify({ status: "OK", coinName: coinName, addressNo: addressNo, addressTo: addressTo, value: value, txid: status.tx }))
+            }else if(error[status]){
+                res.send(JSON.stringify({ status: "Error", type: status, reason: error[status] }))
+            }else{
+                res.send(JSON.stringify({ status: "Error", type: 12, reason: "Something unexpected happened" }))
             }
             break
         default:
             res.send(JSON.stringify({ status: "Error", type: "3", reason: "Coin Name does not exist" }))
     }
 
-}
-
-const errorType = (status) => {
-    if (status == 3) {
-        return "Coin Name does not exist"
-    } else if (status == 4) {
-        return "UTXO server down"
-    } else if (status == 5) {
-        return "UTXO Server unexpected response"
-    } else if (status == 6) {
-        return "Transaction build error"
-    } else if (status == 7) {
-        return "Broadcast server down"
-    } else if (status == 8) {
-        return "Broadcast server unexpected response ***Possible send of money***"
-    } else if (status == 9) {
-        return "Broadcast server unreadable response ***Possible send of money***"
-    } else if (status == 10) {
-        return "Value not set"
-    } else if (status == 11) {
-        return "Amount not available"
-    }
 }
